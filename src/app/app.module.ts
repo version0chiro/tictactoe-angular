efe633ded964cf8407125659f48092184ea63c7f
Chiro <sachinsinghroxx@gmail.com>
2021-09-02 12:01:10 +0530

    added nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
+import {  NbButtonModule } from '@nebular/theme';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +18,12 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,8 +4,16 @@ import { Component, OnInit } from '@angular/core';
 // dump component, no logic only ui
 @Component({
   selector: 'app-square',
-  template: ` <p>{{ value }}</p> `,
-  styles: [],
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">
+      {{ value }}
+    </button>
+  `,
+  styles:  ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
